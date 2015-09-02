bb8696b3e1fe1deaf61e2e68d6efea0b6e0b736e
samnuriu13 <samnuriu13@gmail.com>
2015-09-02 08:37:07 +0600

    COA

--- API/Controls/ucCOA.ascx.designer.cs
+++ API/Controls/ucCOA.ascx.designer.cs
@@ -13,6 +13,24 @@ namespace API.Controls {
     public partial class ucCOA {
         
         /// <summary>
+        /// updatepanel1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel updatepanel1;
+        
+        /// <summary>
+        /// txtInput control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtInput;
+        
+        /// <summary>
         /// tv control.
         /// </summary>
         /// <remarks>
